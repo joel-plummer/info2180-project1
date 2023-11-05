b24c94e201046f23dd744094d1a9f1d264863d8a
joel-plummer <joecodescs@gmail.com>
2023-11-05 13:45:22 -0500

    Edited starter files and added images

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded',function(){
+    var email=document.getElementById("email");
+    var button=document.getElementsByClassName("btn")[1];
+    var message=document.getElementsByClassName("message")[0];
+    button.addEventListener('click',function(event){
+        event.preventDefault();
+        if(email.value==""){
+            message.innerHTML="Please enter a valid email address.";
+        }
+        else{
+            message.innerHTML="Thank you! Your email address "+email.value+" has been added to our mailing list!";
+        }
+    });
+        
+});
